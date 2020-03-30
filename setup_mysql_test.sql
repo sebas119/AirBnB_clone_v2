f2574b903b867d07cc8aeaa5e5293f6a7f40e4bd
danrocus1994 <danrodcastillo1994@gmail.com>
2020-03-29 23:09:25 -0500

    Mysql setup for test user and database

---
+++ setup_mysql_test.sql
@@ -0,0 +1,5 @@
+-- Creates a new user and database  'test' on local host with respecting permissions
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost'; 
