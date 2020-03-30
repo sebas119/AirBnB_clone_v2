be52d9422aee772cd85ff680a8c75a15fc9de38a
danrocus1994 <danrodcastillo1994@gmail.com>
2020-03-29 22:40:42 -0500

    Mysql hbnb_dev user an hbnb_dev_db setting with privileges

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,4 @@
+-- Creates a new user and database on local host with respecting permissions
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost'; 
