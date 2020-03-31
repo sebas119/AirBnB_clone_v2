cbb015bd56973e7ccc33e8ab7726df6eaa535d7d
danrocus1994 <danrodcastillo1994@gmail.com>
2020-03-31 19:12:55 +0000

    Base model correction

--- models/base_model.py
+++ models/base_model.py
@@ -30,9 +30,9 @@ class BaseModel:
 		"""
 		if (len(kwargs) != 0):
 			self.id = kwargs["id"]
-			self.created_at = dt.datetime.strptime(kwargs["created_at"],
+			self.created_at = datetime.strptime(kwargs["created_at"],
 												"%Y-%m-%dT%H:%M:%S.%f")
-			self.updated_at = dt.datetime.strptime(kwargs["updated_at"],
+			self.updated_at = datetime.strptime(kwargs["updated_at"],
 												"%Y-%m-%dT%H:%M:%S.%f")
 			dic = self.__dict__
 			for key in kwargs:
@@ -41,8 +41,8 @@ class BaseModel:
 						dic[key] = kwargs[key]
 		else:
 			self.id = str(uuid.uuid4())
-			self.created_at = dt.datetime.now()
-			self.updated_at = dt.datetime.now()
+			self.created_at = datetime.now()
+			self.updated_at = datetime.now()
 
 	def __str__(self):
 		"""returns a string
