d765ef30f7c9ea420e23b1ebc1f9083b9468f2cf
danrocus1994 <danrodcastillo1994@gmail.com>
2020-04-02 18:00:39 +0000

    Corrected pep8

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -16,42 +16,46 @@ from models.engine.db_storage import DBStorage
 
 
 class TestDBStorage(unittest.TestCase):
-	'''this will test the FileStorage'''
+    '''this will test the FileStorage'''
 
-	@unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") != 'db', "Do test this only if storage is db")
-	@classmethod
-	def setUpClass(cls):
-		"""set up for test"""
-		self.db = MySQLdb.connect(os.getenv("HBNB_MYSQL_HOST"),
-									os.getenv("HBNB_MYSQL_USER"),
-									os.getenv("HBNB_MYSQL_PWD"),
-									os.getenv("HBNB_MYSQL_DB"))
-		self.cursor = self.db.cursor()
+    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") != 'db',
+                     "Do test this only if storage is db")
+    @classmethod
+    def setUpClass(cls):
+        """set up for test"""
+        self.db = MySQLdb.connect(os.getenv("HBNB_MYSQL_HOST"),
+                                  os.getenv("HBNB_MYSQL_USER"),
+                                  os.getenv("HBNB_MYSQL_PWD"),
+                                  os.getenv("HBNB_MYSQL_DB"))
+        self.cursor = self.db.cursor()
 
-	@unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") != 'db', "Do test this only if storage is db")
-	def teardown(cls):
-		"""at the end of the test this will tear it down"""
-		if os.getenv("HBNB_TYPE_STORAGE") == 'db':
-			self.db.close()
+    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") != 'db',
+                     "Do test this only if storage is db")
+    def teardown(cls):
+        """at the end of the test this will tear it down"""
+        if os.getenv("HBNB_TYPE_STORAGE") == 'db':
+            self.db.close()
 
-	@unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") != 'db', "Do test this only if storage is db")
-	def test_pep8_FileStorage(self):
-		"""Tests pep8 style"""
-		style = pep8.StyleGuide(quiet=True)
-		p = style.check_files(['models/engine/db_storage.py'])
-		self.assertEqual(p.total_errors, 0, "fix pep8")
+    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") != 'db',
+                     "Do test this only if storage is db")
+    def test_pep8_FileStorage(self):
+        """Tests pep8 style"""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/engine/db_storage.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
 
-	@unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") != 'db', "Do test this only if storage is db")
-	def test_attributes_DBStorage(self):
-		"""Test DBStorage attributes methods"""
-		self.assertTrue(hasattr(DBStorage, '_DBStorage__engine'))
-		self.assertTrue(hasattr(DBStorage, '_DBStorage__session'))
-		self.assertTrue(hasattr(DBStorage, 'new'))
-		self.assertTrue(hasattr(DBStorage, 'save'))
-		self.assertTrue(hasattr(DBStorage, 'all'))
-		self.assertTrue(hasattr(DBStorage, 'delete'))
-		self.assertTrue(hasattr(DBStorage, 'reload'))
+    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") != 'db',
+                     "Do test this only if storage is db")
+    def test_attributes_DBStorage(self):
+        """Test DBStorage attributes methods"""
+        self.assertTrue(hasattr(DBStorage, '_DBStorage__engine'))
+        self.assertTrue(hasattr(DBStorage, '_DBStorage__session'))
+        self.assertTrue(hasattr(DBStorage, 'new'))
+        self.assertTrue(hasattr(DBStorage, 'save'))
+        self.assertTrue(hasattr(DBStorage, 'all'))
+        self.assertTrue(hasattr(DBStorage, 'delete'))
+        self.assertTrue(hasattr(DBStorage, 'reload'))
 
 
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
