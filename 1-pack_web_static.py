f3d5c6af88b07f311e194ba234d5a6cf22bf2cee
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-04-08 18:22:27 +0000

    Fix pep8

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,4 +1,5 @@
-# Fabric script that generates a .tgz archive from the contents of the web_static folder
+# Fabric script that generates a .tgz archive from
+# the contents of the web_static folder
 
 from fabric.api import local
 from datetime import datetime
