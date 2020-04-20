bde583993ae9dc685c8b7f48a0343567eaf8ba69
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-04-20 20:07:35 +0000

    Working with type variables passed as query params and rendering an HTML page

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,1 +1,45 @@
 #!/usr/bin/python3
+
+"""Flask basic web app"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Start app flask"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """HBNB display"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_isfun(text):
+    """Print params variables"""
+    rep_text = text.replace("_", " ")
+    return 'C %s' % rep_text
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_iscool(text='is_cool'):
+    """Display python is cool by default"""
+    rep_text = text.replace("_", " ")
+    return 'Python %s' % rep_text
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def only_number(n):
+    """Display only if n is integer"""
+    if type(n) == int:
+        return '%d is a number' % n
+
+
+if __name__ == '__main__':
+    app.config['ENV'] = 'development'
+    app.run(host='0.0.0.0', port='5000')
