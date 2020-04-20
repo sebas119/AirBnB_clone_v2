c67848f3630ea7347f6ac2962b51e7d66218fddf
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-04-20 19:47:34 +0000

    Using routes in flask basic

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+"""Flask basic web app"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Start app flask"""
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+
+"""Flask basic web app"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Start app flask"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """HBNB display"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_isfun(text):
+    """Print params variables"""
+    rep_text = text.replace("_", " ")
+    return 'C %s' % rep_text
+
+
+if __name__ == '__main__':
+    app.config['ENV'] = 'development'
+    app.run(host='0.0.0.0', port='5000')
