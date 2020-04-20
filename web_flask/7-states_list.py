7d3de1ebfe8dc8f580a30d41d1d3762c4c431d51
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-04-20 21:57:40 +0000

    States list updated without import State mapping class

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,7 +5,6 @@
 from flask import Flask
 from flask import render_template
 from models import storage
-from models import State
 
 app = Flask(__name__)
 
@@ -19,7 +18,7 @@ def teardown_db(exc):
 @app.route('/states_list', strict_slashes=False)
 def display_html_n_type():
     """Display all states from SQLalchemy in html file"""
-    all_states = storage.all(State)
+    all_states = storage.all('State')
     return render_template('7-states_list.html', states=all_states)
 
 
